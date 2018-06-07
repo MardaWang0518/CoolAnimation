11fda20bd4199609970d8bc2d2a58b23776761d4
wangy@workai.com.cn <wangy@workai.com.cn>
2018-06-07 11:44:07 +0800

    优化

---
+++ app/src/main/java/com/mardawang/android/coolanimation/AnimationView.java
@@ -0,0 +1,201 @@
+package com.mardawang.android.coolanimation;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * author mardawang
+ * <p>
+ * email:wy363681759@163.com
+ * <p>
+ * date: 2018/5/29
+ * <p>
+ * desc: 简单的规律绘制直线构造神奇的视觉效果
+ * <p>
+ * 启发、原理:http://mp.weixin.qq.com/s/FieNhelCar1cZjhBS28ymQ
+ * <p>
+ */
+public class AnimationView extends View {
+    private static final String TAG = "AnimationView";
+    //起点在x、y移动范围
+    private float p1XLength = 400, p1YLength = 20, speedP1 = 0.15f;
+    private float p2XLength = 20, p2YLength = 400, speedP2 = 0.05f;
+    private double angleP1 = 0, angleP2 = 0;
+    private int viewWidth, viewHeight;
+    private Paint paint;
+    private ValueAnimator valueAnimator;
+    //记录移动过的所有点的数据
+    private List<CorrdinateData> corrDatas;
+    private DrawingListener drawingListener;
+    //    private int[] colors = new int[]{Color.RED, Color.WHITE, Color.BLUE};
+    //动画绘制的时间
+    private int animDuration = 12 * 1000;
+
+    public AnimationView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public AnimationView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public AnimationView(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        setBackgroundColor(Color.BLACK);
+        valueAnimator = ValueAnimator.ofFloat(0f, 1f);
+        valueAnimator.setDuration(animDuration);
+        valueAnimator.addUpdateListener(animatorUpdateListener);
+        valueAnimator.addListener(animatorListenerAdapter);
+
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(2);
+        paint.setStyle(Paint.Style.FILL);
+
+//            Shader shader = new LinearGradient(cd.p1X, cd.p1Y, cd.p2X, cd.p2Y, colors, null, Shader.TileMode.MIRROR);
+//            paint.setShader(shader);
+
+        corrDatas = new ArrayList<>();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        for (int i = 0; i < corrDatas.size(); i++) {
+            CorrdinateData cd = corrDatas.get(i);
+            canvas.drawLine(cd.p1X, cd.p1Y, cd.p2X, cd.p2Y, paint);
+        }
+    }
+
+    private ValueAnimator.AnimatorUpdateListener animatorUpdateListener = new ValueAnimator.AnimatorUpdateListener() {
+        @Override
+        public void onAnimationUpdate(ValueAnimator animation) {
+            calculate();
+            invalidate();
+        }
+    };
+
+    private AnimatorListenerAdapter animatorListenerAdapter = new AnimatorListenerAdapter() {
+        @Override
+        public void onAnimationStart(Animator animation) {
+            if (null != drawingListener)
+                drawingListener.drawStart();
+            corrDatas.clear();
+            super.onAnimationStart(animation);
+        }
+
+        @Override
+        public void onAnimationEnd(Animator animation) {
+            if (null != drawingListener)
+                drawingListener.drawOver();
+            super.onAnimationEnd(animation);
+        }
+    };
+
+    /**
+     * 开始绘制
+     */
+    public void startDraw(int pos) {
+        corrDatas.clear();
+        setPaintColor(pos);
+        valueAnimator.start();
+    }
+
+    /**
+     * 计算坐标值
+     */
+    private void calculate() {
+        angleP1 = angleP1 + speedP1;
+        angleP2 = angleP2 + speedP2;
+
+        //两个点的位置更新
+        float nowP1X = (float) (p1XLength * Math.cos(angleP1) + viewWidth / 2f);
+        float nowP1Y = (float) (p1YLength * Math.sin(angleP1) + viewHeight / 2f);
+        float nowP2X = (float) (p2XLength * Math.cos(angleP2) + viewWidth / 2f);
+        float nowP2Y = (float) (p2YLength * Math.sin(angleP2) + viewHeight / 2f);
+
+        CorrdinateData corrdinataData = new CorrdinateData(nowP1X, nowP1Y, nowP2X, nowP2Y);
+        corrDatas.add(corrdinataData);
+    }
+
+    private class CorrdinateData {
+        float p1X, p1Y, p2X, p2Y;
+
+        CorrdinateData(float p1X, float p1Y, float p2X, float p2Y) {
+            this.p1X = p1X;
+            this.p1Y = p1Y;
+            this.p2X = p2X;
+            this.p2Y = p2Y;
+        }
+    }
+
+    /**
+     * 设置参数
+     */
+    public void setParam(float p1XLength, float p1YLength, float p2XLength, float p2YLength, float speedP1, float speedP2) {
+        this.p1XLength = p1XLength;
+        this.p1YLength = p1YLength;
+        this.p2XLength = p2XLength;
+        this.p2YLength = p2YLength;
+        this.speedP1 = speedP1;
+        this.speedP2 = speedP2;
+    }
+
+    private void setPaintColor(int pos){
+        if(paint!=null){
+            switch (pos){
+                case 0:
+                    paint.setColor(Color.WHITE);
+                case 1:
+                    paint.setColor(getResources().getColor(R.color.purple));
+                case 2:
+                    paint.setColor(Color.BLUE);
+                case 3:
+                    paint.setColor(Color.GREEN);
+                case 4:
+                    paint.setColor(getResources().getColor(R.color.orange));
+                case 5:
+                    paint.setColor(Color.RED);
+                default:
+                    paint.setColor(Color.WHITE);
+
+            }
+        }
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        viewWidth = getMeasuredWidth();
+        viewHeight = getMeasuredHeight();
+        p1XLength = p2YLength = viewWidth * 0.8f;
+        p2XLength = p1YLength = viewWidth * 0.04f;
+    }
+
+    public void setDrawingListener(DrawingListener drawingListener) {
+        this.drawingListener = drawingListener;
+    }
+
+    public interface DrawingListener {
+        void drawStart();
+        void drawOver();
+    }
+}
--- app/src/main/java/com/mardawang/android/coolanimation/MainActivity.java
+++ app/src/main/java/com/mardawang/android/coolanimation/MainActivity.java
@@ -1,13 +1,210 @@
 package com.mardawang.android.coolanimation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+
+
+/**
+ * author mardawang
+ * <p>
+ * email:wy363681759@163.com
+ * <p>
+ * date: 2018/5/29
+ * <p>
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mEt_p1x;
+    private EditText mEt_p1y;
+    private EditText mEt_p1_speed;
+    private EditText mEt_p2x;
+    private EditText mEt_p2y;
+    private EditText mEt_p2_speed;
+    private Spinner mSpinner_color;
+    private Spinner mSpinner_type;
+    private TextView mTv_godraw;
+    private AnimationView mAnimate_view;
+    private ArrayList<String> spinnerMenu;
+    private ArrayList<String> spinnerColor;
+    private ArrayList<float[]> drawMenu;
+
+    private boolean firstIn = true;
+    private float[] data1 = {400, 400, 200, 200, 0.05f, 0.35f};
+    private float[] data2 = {200, 500, 500, 200, 0.4f, 0.4f};
+    private float[] data3 = {450, 450, 450, 450, 0.35f, 0.05f};
+    private float[] data4 = {450, 150, 150, 450, 0.1f, 0.05f};
+    private float[] data5 = {450, 450, 450, 450, 0.5f, 0.15f};
+    private float[] data6 = {90, 90, 450, 450, 0.5f, 0.15f};
+    private float[] data7 = {200, 200, 450, 450, 0.55f, 0.15f};
+    private float[] data8 = {450, 150, 150, 450, 0.15f, 0.05f};
+    private float[] data9 = {450, 450, 150, 150, 1f, 0.15f};
+    private int mColor_pos = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initData();
+        initView();
+    }
+
+    private void initView() {
+        mEt_p1x = findViewById(R.id.et_p1x);
+        mEt_p1y = findViewById(R.id.et_p1y);
+        mEt_p1_speed = findViewById(R.id.et_p1_speed);
+
+        mEt_p2x = findViewById(R.id.et_p2x);
+        mEt_p2y = findViewById(R.id.et_p2y);
+        mEt_p2_speed = findViewById(R.id.et_p2_speed);
+
+        mSpinner_color = findViewById(R.id.spinner_color);
+        mSpinner_type = findViewById(R.id.spinner_type);
+        mTv_godraw = findViewById(R.id.tv_godraw);
+        mAnimate_view = findViewById(R.id.animate_view);
+
+        SpinnerAdapter adapter_color = new ArrayAdapter(this, R.layout.item_spinner, spinnerColor);
+        mSpinner_color.setAdapter(adapter_color);
+        mSpinner_color.setSelection(0);
+        mSpinner_color.setOnItemSelectedListener(selectedListener_color);
+
+        SpinnerAdapter adapter_type = new ArrayAdapter(this, R.layout.item_spinner, spinnerMenu);
+        mSpinner_type.setAdapter(adapter_type);
+        mSpinner_type.setSelection(0);
+        mSpinner_type.setOnItemSelectedListener(selectedListener_type);
+
+        mAnimate_view.setDrawingListener(drawingListener);
+
+        mTv_godraw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float p1xLen = Float.valueOf(mEt_p1x.getText().toString());
+                float p1yLen = Float.valueOf(mEt_p1y.getText().toString());
+                float p2xLen = Float.valueOf(mEt_p2x.getText().toString());
+                float p2yLen = Float.valueOf(mEt_p2y.getText().toString());
+                float p1speed = Float.valueOf(mEt_p1_speed.getText().toString());
+                float p2speed = Float.valueOf(mEt_p2_speed.getText().toString());
+                mAnimate_view.setParam(p1xLen, p1yLen, p2xLen, p2yLen, p1speed, p2speed);
+                mAnimate_view.startDraw(mColor_pos);
+            }
+        });
+    }
+
+    private void initData() {
+        spinnerMenu = new ArrayList<>();
+        spinnerMenu.add("一号图案");
+        spinnerMenu.add("二号图案");
+        spinnerMenu.add("三号图案");
+        spinnerMenu.add("四号图案");
+        spinnerMenu.add("五号图案");
+        spinnerMenu.add("六号图案");
+        spinnerMenu.add("七号图案");
+        spinnerMenu.add("八号图案");
+        spinnerMenu.add("九号图案");
+
+        spinnerColor = new ArrayList<>();
+        spinnerColor.add("白色");
+        spinnerColor.add("紫色");
+        spinnerColor.add("蓝色");
+        spinnerColor.add("绿色");
+        spinnerColor.add("橙色");
+        spinnerColor.add("红色");
+
+        drawMenu = new ArrayList<>();
+        drawMenu.add(data1);
+        drawMenu.add(data2);
+        drawMenu.add(data3);
+        drawMenu.add(data4);
+        drawMenu.add(data5);
+        drawMenu.add(data6);
+        drawMenu.add(data7);
+        drawMenu.add(data8);
+        drawMenu.add(data9);
+    }
+
+    private AnimationView.DrawingListener drawingListener = new AnimationView.DrawingListener() {
+        @Override
+        public void drawStart() {
+            mSpinner_type.setClickable(false);
+            mTv_godraw.setTextColor(getResources().getColor(R.color.bg_gray6));
+            mTv_godraw.setClickable(false);
+        }
+
+        @Override
+        public void drawOver() {
+            mTv_godraw.setBackgroundColor(getResources().getColor(R.color.bg_blue));
+            mTv_godraw.setClickable(true);
+            mSpinner_type.setClickable(true);
+        }
+    };
+
+    private AdapterView.OnItemSelectedListener selectedListener_color = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            Log.d("MainActivity", "onItemSelected");
+            if (parent.getId() == R.id.spinner_type) {
+                if (position < 0 || position >= spinnerColor.size())
+                    return;
+
+                if (firstIn) {
+                    firstIn = false;
+                } else {
+                    inflateData(null);
+                    mColor_pos = position;
+                    mAnimate_view.startDraw(mColor_pos);
+                }
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+            Log.d("MainActivity", "onNothingSelected");
+        }
+    };
+
+    private AdapterView.OnItemSelectedListener selectedListener_type = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            Log.d("MainActivity", "onItemSelected");
+            if (parent.getId() == R.id.spinner_type) {
+                if (position < 0 || position >= drawMenu.size())
+                    return;
+
+                if (firstIn) {
+                    firstIn = false;
+                } else {
+                    inflateData(drawMenu.get(position));
+                    mAnimate_view.startDraw(mColor_pos);
+                }
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+            Log.d("MainActivity", "onNothingSelected");
+        }
+    };
+
+    private void inflateData(float[] datas) {
+        if (datas == null) {
+            datas = data1;
+        }
+        mEt_p1x.setText(datas[0] + "");
+        mEt_p1y.setText(datas[1] + "");
+        mEt_p2x.setText(datas[2] + "");
+        mEt_p2y.setText(datas[3] + "");
+        mEt_p1_speed.setText(datas[4] + "");
+        mEt_p2_speed.setText(datas[5] + "");
+        mAnimate_view.setParam(datas[0], datas[1], datas[2], datas[3], datas[4], datas[5]);
+
     }
 }
